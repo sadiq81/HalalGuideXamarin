573f8b417de67f9e4922084561ed8e80d5b2abb6
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2014-07-20 18:42:18 +0200

    init

---
+++ HalalGuide.iOS/HalalGuide.iOSViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace HalalGuide.iOS
+{
+	[Register ("HalalGuide_iOSViewController")]
+	partial class HalalGuide_iOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
